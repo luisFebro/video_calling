42e95a34a63c5286ec908efbb156c3cbe98d9c94
lufebro <mr.febro@gmail.com>
2026-02-28 20:37:47 -0300

    added socket.io server

---
+++ server/dist/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+const port = 8080;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+io.on("connection", (socket) => {
+    console.log("A user connected");
+});
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+//# sourceMappingURL=index.js.map
