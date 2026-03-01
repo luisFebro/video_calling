6507f9f22851a41a26a324cc5ef25cb526b52f46
lufebro <mr.febro@gmail.com>
2026-03-01 15:57:04 -0300

    added peerId in the back

--- server/dist/room/index.js
+++ server/dist/room/index.js
@@ -1,12 +1,14 @@
 import { v4 as uuidv4 } from "uuid";
+const rooms = {};
 export const roomHandler = (socket) => {
     const createRoom = () => {
         const roomId = uuidv4();
+        rooms[roomId] = [];
         // TODO: save to consulta item.
         socket.emit("room-created", { roomId });
         console.log("A user created a room");
     };
-    const joinRoom = ({ roomId }) => {
+    const joinRoom = ({ roomId, peerId }) => {
         socket.join(roomId);
         console.log("A user joined the room", roomId);
     };
--- server/src/room/index.ts
+++ server/src/room/index.ts
@@ -1,16 +1,24 @@
 import { Socket } from "socket.io";
 import { v4 as uuidv4 } from "uuid";
 
+const rooms: Record<string, string[]> = {};
+interface IRoomParams {
+    roomId: string;
+    peerId: string;
+}
+
 export const roomHandler = (socket: Socket) => {
     const createRoom = () => {
         const roomId = uuidv4();
+
+        rooms[roomId] = [];
 
         // TODO: save to consulta item.
         socket.emit("room-created", { roomId });
         console.log("A user created a room");
     };
 
-    const joinRoom = ({ roomId }: { roomId: string }) => {
+    const joinRoom = ({ roomId, peerId }: IRoomParams) => {
         socket.join(roomId);
         console.log("A user joined the room", roomId);
     };
